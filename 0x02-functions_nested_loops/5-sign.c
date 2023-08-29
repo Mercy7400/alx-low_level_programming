5e6aed8f17314467a535552db94b831d256ddd8a
Mercy7400 <nyokabikamau100@gmail.com>
2023-08-29 11:34:39 -0700

    0x0B. C - malloc, free

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,18 +1,36 @@
-#include "main.h"
+#include <stdio.h>
 
-/**
- * print_sign - prints the sign of a number
- * @n: the int to check
- * Return: 1 and prints + if n is greater than zero
- * 0 and prints 0 if n is zero
- * -1 and prints - if n is less than zero
- */
-int print_sign(int n)
-{
-	if (n > 0)
-	{
-		_putchar('+');
-		return (1);
-	} else if (n == 0)
+int print_sign(int n) {
+    if (n > 0) {
+        printf("+\n");
+        return 1;
+    } else if (n == 0) {
+        printf("0\n");
+        return 0;
+    } else {
+        printf("-\n");
+        return -1;
+    }
+}
+
+int main() {
+    int num;
+    int result; /* Move the variable declaration to the top of the function*/
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    result = print_sign(num); / Assign the result of the function to the variable
+
+    /* Use the variable 'result' to check the sign in the rest of your program if needed */
+    if (result == 1) {
+        printf("The number is positive!\n");
+    } else if (result == 0) {
+        printf("The number is zero!\n");
+    } else {
+        printf("The number is negative!\n");
+    }
+
+    return 0;
 }
 
